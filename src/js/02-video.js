d560bc89d7516069dca914de5f14873fcc4f9306
Vashchenia Leonid <dcs.lvv.87@gmail.com>
2022-09-25 23:00:00 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// import localStorage from './storage';
+import { save, load } from './storage';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const playerRef = document.querySelector('#vimeo-player');
+
+const player = new Player(playerRef);
+
+function savePlayTime(data) {
+  save(CURRENT_TIME_KEY, Math.floor(data.seconds));
+}
+
+player.on('timeupdate', throttle(savePlayTime, 1000));
+
+let loadedTime = load(CURRENT_TIME_KEY);
+if (loadedTime) {
+  player.setCurrentTime(loadedTime);
+}
