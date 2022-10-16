e800b35c366f6f7ef0502b9b4828330fbd6a972b
LeonidVashchenia <46425191+LeonidVashchenia@users.noreply.github.com>
2022-10-16 13:14:39 +0000

    Deploying to gh-pages from @ LeonidVashchenia/parcel-08@4e4d29935562303a088509a45b5a931409390aab 🚀

---
+++ 04-test.8ea859af.js
@@ -0,0 +1,2 @@
+!function(){var n=setInterval((function(n){console.log("Лог кожні ".concat(n," мс - ").concat(Date.now()," !"))}),1e3,1e3);setTimeout((function(){return clearInterval(n)}),5e3)}();
+//# sourceMappingURL=04-test.8ea859af.js.map
---
+++ 04-test.967385d1.js
@@ -0,0 +1,2 @@
+const e=setInterval((e=>{console.log(`Лог кожні ${e} мс - ${Date.now()} !`)}),1e3,1e3);setTimeout((()=>clearInterval(e)),5e3);
+//# sourceMappingURL=04-test.967385d1.js.map
